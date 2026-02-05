182e9b36e2f3242912073b29d2c69c1ec4ee10c8
Mohammed Alkhulaifi <3075556A@student.gla.ac.uk>
2026-02-05 21:21:28 +0000

    Chapter 9 completed, test working.  User authentication, registration, login/logout, and access restrictions done

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -29,9 +31,20 @@ class PageForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        # If url is not empty and doesn't start with 'http://' or 'https://',
-        # then prepend 'http://'.
         if url and not (url.startswith('http://') or url.startswith('https://')):
             cleaned_data['url'] = f'http://{url}'
 
         return cleaned_data
+    
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
